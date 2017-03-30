24b792c308a58ced0404408ba2f8bdb6799fd3e0
Niklas Johannesson <nj@searchminds.se>
2017-03-30 14:48:30 +0000

    :red_circle: Done for today
    Still working on searchview and fleshing out the website

--- Core-files/View/SearchView.php
+++ Core-files/View/SearchView.php
@@ -2,7 +2,27 @@
     class SearchView{
         
         public function htmlResponse(){
+            
             return'
+            
+            <div id="fullpage">
+            
+            	<div class="row section">
+            	    <div class="col s4 push-s5">
+            	    
+            	    </div>
+            	</div>
+            	
+            	<div class="row section">
+            	
+            	</div>
+            	
+            	<div class="row section">
+            	
+            	</div>
+            	
+            </div>
+            
             ';
         }
         
