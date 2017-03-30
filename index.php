3b950a6e076fd20ab30189e2d0ea0b54fcc81fff
Niklas Johannesson <nj@searchminds.se>
2017-03-30 09:14:08 +0000

    :white_check_mark: Added require on the files
    Gona need to add autoloader in the future to increase stability and easy-to-read code

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+//------Require_once, Files
+
+//REMINDER: Gona have to use some autoloader here later.
+
+//Controller
+require_once("Core-Files/Controller/BrowserController.php");
+require_once("Core-Files/Controller/InfoController.php");
+require_once("Core-Files/Controller/MainController.php");
+require_once("Core-Files/Controller/RegController.php");
+require_once("Core-Files/Controller/SearchController.php");
+//Model
+require_once("Core-Files/Model/BrowserModel.php");
+require_once("Core-Files/Model/InfoModel.php");
+require_once("Core-Files/Model/RegModel.php");
+require_once("Core-Files/Model/SearchModel.php");
+//View
+require_once("Core-Files/View/BrowserView.php");
+require_once("Core-Files/View/InfoView.php");
+require_once("Core-Files/View/MainView.php");
+require_once("Core-Files/View/RegView.php");
+require_once("Core-Files/View/SearchView.php");
+//Session
+require_once("Core-Files/Session/Session.php");
+//Database
+require_once("Core-Files/Database/Database.php");
+
+//------
