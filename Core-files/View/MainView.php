3dd33514c5f4128d220905f5a966228cb27251e2
Niklas Johannesson <nj@searchminds.se>
2017-03-30 12:03:38 +0000

    :white_check_mark: Comments
    Updated MainView with comments, also changed the description and goal of screenRender to better fit it

--- Core-files/View/MainView.php
+++ Core-files/View/MainView.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * MainView is the holder class for one of the main functions of the website, screenRender.
+ * It will hold the base template of the HTML 5 Code that will serve as the front page.
+ * it will be synced with a helper function that will server as the flow and controller
+ * to direct the right content to the right place, think of it as a router
+**/ 
     class MainView{
         
         public function screenRender(){
